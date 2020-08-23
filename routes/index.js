2c583671a11bab0ac60ad7f96ad07c1eed40fb2a
Brad Rogers <brad12rogers@gmail.com>
2020-08-22 22:30:53 -0700

    Add summary route and update postman schema

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,35 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/summary', function (req, res, next) {
+  db.select_accounts((err, rows) => {
+    if (err) {
+      translate_error(err, res);
+    }
+    account_result = `Accounts: ${JSON.stringify(rows)} `;
+    db.select_categories((err, rows) => {
+      if (err) {
+        translate_error(err, res);
+      }
+      category_result = `Categories: ${JSON.stringify(rows)} `;
+      db.select_transactions((err, rows) => {
+        if (err) {
+          translate_error(err, res);
+        }
+        transaction_result = `Transactions: ${JSON.stringify(rows)} `;
+        res.send(account_result + category_result + transaction_result);
+      });
+    });
+  });
+});
+
 router.get('/close', function (req, res, next) {
-  db.close((err) => translate_error(err, res));
-  res.send("close success");
+  db.close((err) => {
+    if (err) {
+      translate_error(err, res);
+    }
+    res.send("close success");
+  });
 });
 
 module.exports = router;
