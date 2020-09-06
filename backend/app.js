08b509e4f5a68218ad852baad11a0d46389d11a9
Brad Rogers <brad12rogers@gmail.com>
2020-09-05 18:01:43 -0700

    Fix flag

--- backend/app.js
+++ backend/app.js
@@ -48,6 +48,7 @@ app.use(function (err, req, res, next) {
 });
 
 // Setup DB based off env flag
+console.log(process.env.SETUP_DB);
 if (process.env.SETUP_DB !== 0) {
   console.log('Initializing DB');
   db.initialize_db(db_file);
